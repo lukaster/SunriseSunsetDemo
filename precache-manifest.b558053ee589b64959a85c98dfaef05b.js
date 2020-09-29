ca30b32b7395e13097c070653ab54e87e4dfbfb8
tlukasova <tereza.lukasova@neuronsw.com>
2020-09-29 19:33:28 +0200

    Updates

---
+++ precache-manifest.b558053ee589b64959a85c98dfaef05b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca30b0c62cb3fd667834825afb2ff86f",
+    "url": "/SunriseSunsetDemo/index.html"
+  },
+  {
+    "revision": "60a8da935af5a03bc4b5",
+    "url": "/SunriseSunsetDemo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e19702a2d68c69c03bb8",
+    "url": "/SunriseSunsetDemo/static/js/2.7fbd71c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SunriseSunsetDemo/static/js/2.7fbd71c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a8da935af5a03bc4b5",
+    "url": "/SunriseSunsetDemo/static/js/main.e588fa1e.chunk.js"
+  },
+  {
+    "revision": "836b0aeaa4de1a42be9c",
+    "url": "/SunriseSunsetDemo/static/js/runtime-main.64aaaaf0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SunriseSunsetDemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
